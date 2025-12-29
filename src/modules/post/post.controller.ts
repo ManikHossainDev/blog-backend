fe21892f4c94378d4c8c99ef2937fb90c20e641c
Programmer-Manik <programmermanik1502083874@gmail.com>
2025-12-30 00:30:50 +0600

    Implementing Create Post API

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,18 @@
+import { Result } from './../../../generated/prisma/internal/prismaNamespace';
 import { Request, Response } from "express";
+import { postService } from './post.service';
+import { Post } from '../../../generated/prisma/client';
 
 const createPost = async (req:Request, res:Response) => {
-   console.log({req, res})
+   try{
+        const result = await postService.createPost(req.body)
+        res.status(201).json(result)
+   }catch(err){
+     res.status(400).json({
+       error: "post creation failed",
+       details: err,
+     });
+   }
 };
 
 export const PostController = {
