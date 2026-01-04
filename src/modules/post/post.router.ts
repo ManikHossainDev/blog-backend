c5e813667a43438242d47ac1411dd4c1fc60c97b
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-04 20:09:03 +0600

    update

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,7 +1,21 @@
-import express, { Router } from "express";
+
+
+import express, { NextFunction, Request, Response, Router } from "express";
 import { PostController } from "./post.controller";
+import { auth as betterAuth } from "../../lib/auth";
 const router = express.Router();
+const auth = (...roles:any) => {
+   return async (req:Request, res:Response, next:NextFunction) => {
+       // get user session from request
+       const session = await betterAuth.api.getSession({
+          headers:req.headers as any
+       });
+       console.log(session)
+    }
+}
+
 router.post("/", 
+    auth("USER"),
     PostController.createPost
 );
 export const postRouter: Router = router;
