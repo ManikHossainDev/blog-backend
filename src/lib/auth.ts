2f598ca96d0b6cf7d685f7f21643523b9c601267
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-04 13:39:20 +0600

    additionalfields

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,28 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
 
-
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+   user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: "USER",
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: "ACTIVE",
+      }
+    }
+   },
   emailAndPassword: {
     enabled: true,
   },
