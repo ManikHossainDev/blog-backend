55dd8fb524a129fe1dcd30e5d230cddab15a52ee
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-06 00:58:00 +0600

    Filtering Posts by Tags

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,44 +1,60 @@
-import { Payload } from './../../../generated/prisma/internal/prismaNamespace';
+import { Payload } from "./../../../generated/prisma/internal/prismaNamespace";
 import { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const createPost = async (data: Omit<Post, "id" | "createdAt" | "updatedAt">, userId: string) => {
-    const result = await prisma.post.create({
-        data: {
-            ...data,
-            authorId: userId
-        }
-    })
-    return result;
+const createPost = async (
+  data: Omit<Post, "id" | "createdAt" | "updatedAt">,
+  userId: string
+) => {
+  const result = await prisma.post.create({
+    data: {
+      ...data,
+      authorId: userId,
+    },
+  });
+  return result;
 };
 
-const getAllPosts = async (payload:{search: string | undefined}) => {
-    const allposts = await prisma.post.findMany({
-        where: {
-            OR: [
-                {
-                    title: {
-                contains: payload.search as string,
-                mode: "insensitive"
-            },
-                },
-            {content: {
-                contains: payload.search as string,
-                mode: "insensitive"
-            }},
+const getAllPosts = async (payload: {
+  search: string ;
+  tags: string[] | [];
+}) => {
+  const allposts = await prisma.post.findMany({
+    where: {
+      AND: [
+       payload.search &&  {
+          OR: [
             {
-                tags: {
-                    has: payload.search as string   
-                }
-            }
-            ]
-        }
-    });
-    return allposts;
-}
-
+              title: {
+                contains: payload.search,
+                mode: "insensitive",
+              },
+            },
+            {
+              content: {
+                contains: payload.search,
+                mode: "insensitive",
+              },
+            },
+            {
+              tags: {
+                has: payload.search,
+              },
+            },
+          ],
+        },
+        {
+          tags: {
+            hasSome: payload.tags,
+          },
+        },
+      ],
+    },
+  });
+  return allposts;
+};
 
 export const postService = {
-    createPost,
-    getAllPosts
-}
+  createPost,
+  getAllPosts,
+};
