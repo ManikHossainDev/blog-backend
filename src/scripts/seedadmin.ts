5e59b85d86ad3a508714f8d717918952cbc6501f
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-07 01:30:13 +0600

    Admin Seeding Part

---
+++ src/scripts/seedadmin.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middlewares/auth";
+
+
+async function  seedAdmin () {
+    try {
+      const adminData = {
+        name: "Admin User",
+        email: "admin@gmail.com",
+        role: UserRole.ADMIN,
+        password: "admin123", 
+      };
+      // check user exist on db or not 
+      const  existingUser = await prisma.user.findUnique({
+        where: { email: adminData.email },
+      })  
+
+        if (existingUser) {
+            throw new Error("Admin user already exists");
+        }
+
+        const signUpAdmin = await fetch("http://localhost:3000/api/auth/sign-up/email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(adminData),
+        });
+
+        
+
+       
+
+    } catch (error) {
+        console.error("Error seeding admin user:", error);
+    }
+}
