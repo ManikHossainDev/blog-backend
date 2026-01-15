3b2d637e192e97e4f708786c4b2796c1072324ef
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-16 02:47:59 +0600

    Handling Prisma Client Validation Errors

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
 
 function errorHandler(
   err: any,
@@ -6,13 +7,21 @@ function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  res.status(500);
-  res.json({
-    message:"error from error handler",
-    error:err
-  })
-//   res.render("error from error handler :-", { error: err });
+  let statusCode = 500;
+  let errorMessage = "Internal Server Error";
+  let errorDetails = err ;
+  
+  // PrismaClientValidationError
+  if(err instanceof Prisma.PrismaClientInitializationError){
+      statusCode = 400;
+      errorMessage = "you provide incorrect field type or missing fields!"
+  }
 
+  res.status(statusCode);
+  res.json({
+    message: errorMessage,
+    error:errorDetails
+  })
 }
 
 export default errorHandler
