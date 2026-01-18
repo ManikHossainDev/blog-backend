95f7a91f20c739d4367677955a0edbf7e34ec4bd
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-18 19:24:59 +0600

    manik hoosain console.log

--- blog-backend/src/server.ts
+++ blog-backend/src/server.ts
@@ -9,6 +9,7 @@ async function main() {
         console.log("Connected to the database successfully.");
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`)
+            console.log('manik hossain')
         })
     }catch(error){
         console.error("An error occurred", error);
