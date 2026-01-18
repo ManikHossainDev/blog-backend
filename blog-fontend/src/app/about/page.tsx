94471977743888a4fc27393fbca5961dbeb80c43
Programmer-Manik <programmermanik1502083874@gmail.com>
2026-01-18 23:50:09 +0600

    Creating Basic Routes and Understanding Core Layout Concepts

---
+++ blog-fontend/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+ return (
+ <div>
+ <h2>Welcome to the Page page about</h2>
+ </div>
+ );
+};
+
+export default Page;
---
+++ blog-fontend/src/app/contact/branch/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+ return (
+ <div>
+ <h2>Welcome to the Page page branch</h2>
+ </div>
+ );
+};
+
+export default Page;
